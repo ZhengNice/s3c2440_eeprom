cff8fe44b6ad846220a04d9eb3a307639d1c75e4
wwl <wwl@centos6.localdomain>
2015-11-27 21:07:41 +0800

    test.c

---
+++ test.c
@@ -0,0 +1,14 @@
+/*********************************************************************************
+ *      Copyright:  (C) 2015 Zhu Shizheng
+ *                  All rights reserved.
+ *
+ *       Filename:  test.c
+ *    Description:  This file 
+ *                 
+ *        Version:  1.0.0(2015年11月27日)
+ *         Author:  Zhu Shizheng <shizhengzhu@126.com>
+ *      ChangeLog:  1, Release initial version on "2015年11月27日 21时06分19秒"
+ *                 
+ ********************************************************************************/
+
+hello world
